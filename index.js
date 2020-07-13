3d0960ac88f7964b7188e360c6bcc8eb836fc204
alexpac27 <alexpac27@gmail.com>
2020-07-12 22:13:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+const array = ["jill","ben","amanda"];
+const event = "birthday";
+
+function writeCards(array, event) {
+    let thanks = []
+     for (let i = 0; i < array.length; i++){
+        thanks.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thanks;
+}
+
+writeCards(array, event);
+
+
+function countDown( num ) {
+    while ( num > 0 ) {
+      console.log( num );
+      num -= 1;
+    }
+    console.log( num );
+  }
+  
+
+// function countDown(num){
+//     let i = num;
+//     while (i > -1){
+//     console.log(`${i}`)
+//     i -= 1;}
+// }
+
+countDown(3);
+
